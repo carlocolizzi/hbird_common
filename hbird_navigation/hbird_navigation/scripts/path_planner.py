4a5059fb0ca0dac48a396f938ee26a914892f642
Carlo <49639887+carlocolizzi@users.noreply.github.com>
2023-09-30 18:32:42 -0400

    Update path_planner.py

--- hbird_navigation/hbird_navigation/scripts/path_planner.py
+++ hbird_navigation/hbird_navigation/scripts/path_planner.py
@@ -1,4 +1,5 @@
 from hbird_msgs.msg import Waypoint
+from queue import PriorityQueue
 import math
 
 class PathPlanner():
@@ -14,10 +15,25 @@ class PathPlanner():
                          (see Environment class in planner_utils.py)
         """
         self.env = env
-
-        # your code here
-
         
+        self.open = PriorityQueue() ### FIND WAY OF SORTING QUEUE
+
+        self.open.put(self.env.start_pos)
+
+        self.closed = []
+        
+
+    def get_neighbours(self, node):
+        x = node.x
+        y = node.y 
+
+        neighbours = [[x-self.env.grid_size, y - self.env.grid_size], [x-self.env.grid_size, y], [x-self.env.grid_size, y + self.env.grid_size],
+                      [x, y - self.env.grid_size], [x, y + self.env.grid_size],
+                      [x+self.env.grid_size, y - self.env.grid_size], [x+self.env.grid_size, y], [x+self.env.grid_size, y + self.env.grid_size],]
+        
+        return neighbours
+
+
     def plan(self):
         """
         Main method that computes and returns the path
@@ -25,22 +41,73 @@ class PathPlanner():
         Returns:
         - path (list of Waypoint objects)      
         """
+        distance = []
+        
+        while self.open:
+            node = self.open.get()
+            self.closed.append(node)
+
+            if node == self.env.goal_pos:
+                return self.open 
+            else:
+                neighbours = get_neighbours(node) 
+
+            for element in neighbours:
+                element.parent = node
+                distance.x = self.env.goal_pos.position.x - element.x
+                distance.y = self.env.goal_pos.position.y - element.y
+                element.h = math.sqrt(distance.x **2 + distance.y **2)
+                element.g = node.g + math.sqrt(node.x-element.x ** 2 + node.y - element.y ** 2)
+                element.f = element.g + element.h
+                
+                if element in self.open and element.f > self.open.f: ### need to fidn out how to find element in queue
+                    continue
+                if element in self.closed and element.f > self.closed.f: ### need to fidn out how to find element in queue
+                    continue
+                self.open.get(element)
+                self.closed.get(element)
+                self.open.put(element)
+    
+        return None
+
+            
+
+
+        '''
+        while current_pos != self.env.goal_pos:
+            node = self.open.f.index(min(self.open.f))
+            
+            if node == self.env.goal_pos:
+                ## DONE
+            else:
+                self.closed.append(node)
+                neighbours = get_neighbours(node)
+            for element in neighbours:
+                if element.g < node.g and element in self.closed:
+                    self.closed
+        
         
         # for each square
+
+        
         for x in range(self.env.x_min + (self.env.grid_size/2), self.env.x_max, self.env.grid_size):
             for y in range(self.env.y_min + (self.env.grid_size/2), self.env.y_max, self.env.grid_size):
+        
+
+        for x in range(current_pos[x] - self.env.grid_size, current_pos[x] + self.env.grid_size, self.env.grid_size):
+            for y in range(current_pos[y] - self.env.grid_size, current_pos[y] + self.env.grid_size, self.env.grid_size):
             
             # ensure there are no obstacles in this square
             if x in self.env.obstacles and y in self.env.obstacles:
                 
-                array1[] = list(np.arange(pair[current_pos[x - self.env.robot_radius]][current_pos[y - self.env.robot_radius]], pair[current_pos[x + self.env.robot_radius]][current_pos[y + self.env.robot_radius]], 0.1
+                # array1[] = list(np.arange(pair[current_pos[x - self.env.robot_radius]][current_pos[y - self.env.robot_radius]], pair[current_pos[x + self.env.robot_radius]][current_pos[y + self.env.robot_radius]], 0.1
                 
-                x_radius_max = current_pos[x] + robot_radius
-                x_radius_min = current_pos[x] - robot_radius
-                y_radius_max = current_pos[y] + robot_radius
-                y_radius_min = current_pos[y] - robot_radius
+                x_radius_max = current_pos[x] + self.env.robot_radius
+                x_radius_min = current_pos[x] - self.env.robot_radius
+                y_radius_max = current_pos[y] + self.env.robot_radius
+                y_radius_min = current_pos[y] - self.env.robot_radius
                 
-                if self.env.obstacles['x'] < x_radius_max and self.env.obstacles['x'] > x_radius_min:
+                if any(self.env.obstacles['x'] < x_radius_max and self.env.obstacles['x'] > x_radius_min:
                     if self.env.obstacles['y'] < y_radius_max and self.env.obstacles['y'] > y_radius_min:
                         grid[x][y] = 999
 
@@ -61,14 +128,11 @@ class PathPlanner():
 
             # add distance to target + distance to drone
             grid[x][y] += distance.sum
+        '''
 
 
-
-        path = []
-
-        # your code here
-
+        # path = 
         
-        return path
+        # return path
 
 
